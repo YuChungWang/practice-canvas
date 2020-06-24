e02766fa7ac424d8d2c6780899dd05ed066520b1
Charlie Wang <wangch@garena.com>
2020-06-24 10:09:46 +0800

    canvas init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/icon-canvas.jpg" class="icon">
+    <hello-canvas />
+  </div>
+</template>
+
+<script>
+import HelloCanvas from './components/HelloCanvas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloCanvas
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.icon {
+  width: 60px;
+  height: 60px;
+}
+</style>
---
+++ src/components/HelloCanvas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello-canvas">
+    <h1>Hello Canvas</h1>
+    <canvas id="canvas" width="600" height="400" ref="canvas" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloCanvas',
+
+  mounted() {
+    const ctx = this.$refs.canvas.getContext('2d');
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
