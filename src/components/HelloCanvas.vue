ae287f0e207bb652d7add1c54f2c10ff1c5a3420
Charlie Wang <wangch@garena.com>
2020-12-07 16:00:27 +0800

    canvas sprite animation

--- src/components/HelloCanvas.vue
+++ src/components/HelloCanvas.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello-canvas">
-    <h1>Hello Canvas</h1>
-    <canvas id="canvas" width="600" height="400" ref="canvas" />
+    <h2>Hello Canvas</h2>
+    <canvas id="canvas" width="1334" height="750" ref="canvas" />
   </div>
 </template>
 
@@ -9,13 +9,52 @@
 export default {
   name: 'HelloCanvas',
 
-  mounted() {
-    const ctx = this.$refs.canvas.getContext('2d');
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 55, 50);
+  methods: {
+    gameLoop() {
+      if(Date.now() - this.lastUpdate >= this.timePerFrame) {
+        this.frameIndex++;
+        if(this.frameIndex >= this.numberOfFrames) {
+            this.frameIndex = 0;
+        }
+        this.lastUpdate = Date.now();
+      }
 
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (30, 30, 55, 50);
+      if (this.frameIndex > this.maxFrame) {
+        this.frameIndex = 0;
+      }
+      this.ctx.drawImage(
+        this.img,
+        0,
+        this.frameIndex * this.frameHeight,
+        this.frameWidth,
+        this.frameHeight,
+        0,
+        0,
+        this.frameWidth,
+        this.frameHeight,
+      );
+      
+      
+      window.requestAnimationFrame(this.gameLoop);
+    }
+  },
+
+  mounted() {
+    this.ctx = this.$refs.canvas.getContext('2d');
+
+    this.img = new Image();
+    this.img.src = 'https://dlgarenanow-a.akamaihd.net/mgames/kgtw/events/202007TelCgacha/cdn/animation/animation-arrow-light.jpg';
+
+    this.frameWidth = 1334;
+    this.frameHeight = 750;
+    this.numberOfFrames = 25;
+    this.frameIndex = 0;
+    this.lastUpdate = 0;
+    this.timePerFrame = 20;
+
+    this.img.onload = () => {
+      this.gameLoop();
+    }
   }
 }
 </script>
